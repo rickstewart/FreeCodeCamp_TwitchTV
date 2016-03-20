2e7350d4c81b816d8cd45f004ec4a46f813198ad
rickstewart <rick.stewart@ns.sympatico.ca>
2016-03-19 22:17:08 -0300

    basic html and css layout complete.

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Rick Stewart on 03/19/2016.
+ */
+/**
+ * set JSHint not to flag these variables as 'unresolved variable'
+ * @param data.query.pages
+ *
+ */
+'use strict';
+function wikipediaViewerMain() {
+}
+
+/* function runs main script when page has loaded. Program entry point. */
+$(document).ready(function () {
+	wikipediaViewerMain();
+});
+
